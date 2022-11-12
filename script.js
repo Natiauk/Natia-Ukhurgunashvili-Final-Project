84027efd286cbdaa46c7154df7303a50eb1ce73d
Natiauk <115416000+Natiauk@users.noreply.github.com>
2022-11-13 01:05:32 +0400

    added burgers

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let burger = document.querySelector(".fa-bars");
+let mobileMenu = document.querySelector(".Nav1_ul");
+
+burger.addEventListener("click", function(){
+    mobileMenu.classList.toggle("active");
+})
